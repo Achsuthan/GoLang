ebadbea8f79d0dcda1b84bc2392f36ed4f050fd7
Achsuthan <achsuthan@pikanite.com>
2018-07-10 09:28:27 +0530

    first package done

--- 03_Package/main/main.go
+++ 03_Package/main/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"Achsuthan/03_Package/stringutil"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("hello Package")
-	fmt.Println("hello ")
+	fmt.Println("hello ", stringutil.UserName)
 }
