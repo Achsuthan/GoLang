9542ef9204a6228067ff9b22cce71ee86ebbec0f
Achsuthan <achsuthan@pikanite.com>
2018-07-10 09:13:11 +0530

    number format and forloop added

---
+++ 02_NumbersAndLoops/numbers.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//printing the numbers in decimal binary and hexadecimal
+	//printf is used to print the custom print
+	fmt.Printf("%d \t %b \t %x \n", 10, 10, 10)
+
+	//loop
+
+	for i := 0; i < 10; i++ {
+		fmt.Println("Hello number : ", i)
+	}
+}
